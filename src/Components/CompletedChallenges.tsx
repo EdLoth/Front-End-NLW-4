1e48df14e9a2ff148d1bb579624f9680deffadd6
Eduardo Ramos <57899212+EdLoth@users.noreply.github.com>
2021-03-16 13:57:03 -0300

    CompletedChalanges is completed :white_check_mark:

--- src/Components/CompletedChallenges.tsx
+++ src/Components/CompletedChallenges.tsx
@@ -1,10 +1,10 @@
 import { useContext } from 'react';
 import { ChallangesContext } from '../contexts/ChallangesContent';
+
 import styles from '../styles/components/CompletedChallenges.module.css';
 
 export default function CompletedChallanges() {
     const { challangesCompleted } = useContext(ChallangesContext);
-
     return (
         <div className={styles.completedChallengesContainer}>
             <span>Desafios Completos: </span>
